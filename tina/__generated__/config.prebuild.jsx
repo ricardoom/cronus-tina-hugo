ba9cfc9abc060917c1fb9f7799b1d306a510b793
ricardoom <ricardo.e.galvez@gmail.com>
2024-04-24 15:14:16 -0700

    feat(General): :zap: change to env to dev for the dev branch.

--- tina/__generated__/config.prebuild.jsx
+++ tina/__generated__/config.prebuild.jsx
@@ -1,6 +1,6 @@
 // tina/config.ts
 import { defineConfig } from "tinacms";
-var branch = process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || process.env.HEAD || "main";
+var branch = process.env.GITHUB_BRANCH || process.env.VERCEL_GIT_COMMIT_REF || process.env.HEAD || "dev";
 var config_default = defineConfig({
   branch,
   // Get this from tina.io
